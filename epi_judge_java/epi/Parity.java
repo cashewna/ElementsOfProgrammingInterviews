d4d7e797628a9da57396e3b51af0d7115af0b753
Krishna Khong <krishnakhong@gmail.com>
2024-05-19 15:19:28 +0800

    feat: implement ParityBits

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -1,18 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    // time complexity = O(n)
+    //    short result = 0;
+    //    while (x != 0) {
+    //      result ^= (short) (x & 1);
+    //      x = x >> 1;
+    //    }
+    //    return result;
+
+    // time complexity = O(k) where k is the number of bits set to 1
+//    short result = 0;
+//    while (x != 0) {
+//      result ^= 1;
+//      x &= (x - 1); // drops the lowest set bit of x
+//    }
+//    return result;
+
+    // time complexity = O(log n)
+    x ^= x >>> 32;
+    x ^= x >>> 16;
+    x ^= x >>> 8;
+    x ^= x >>> 4;
+    x ^= x >>> 2;
+    x ^= x >>> 1;
+    return (short)(x & 0x1);
   }
 
   public static void main(String[] args) {
     System.exit(
-        GenericTest
-            .runFromAnnotations(args, "Parity.java",
-                                new Object() {}.getClass().getEnclosingClass())
+        GenericTest.runFromAnnotations(
+                args, "Parity.java", new Object() {}.getClass().getEnclosingClass())
             .ordinal());
   }
 }
